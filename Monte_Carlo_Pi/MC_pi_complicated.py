ac7d2ba044e0a0bbbf73a071fde8f16f7e22d371
Joel A. Kulesza <jkulesza@gmail.com>
2016-04-07 11:07:58 -0400

    Added some comments, more to come

--- Monte_Carlo_Pi/MC_pi_complicated.py
+++ Monte_Carlo_Pi/MC_pi_complicated.py
@@ -3,8 +3,10 @@ from MC_pi_simple import calc_pi
 from matplotlib import pyplot as plt
 from scipy import interpolate as interp
 
+################################################################################
 # Create scatter plot of individual points, styled by whether they are within or
 # outside the circle.
+################################################################################
 n = 2500
 x, y, pi, relerr = calc_pi(n)
 fig = plt.figure()
@@ -25,7 +27,9 @@ plt.title(r'$\pi$ estimated as ' + '{:.5f}'.format(pi) + ' using ' + str(n) + ' 
 plt.axes().set_aspect('equal')
 plt.savefig('plot_complicated_MC_pi_points.png')
 
+################################################################################
 # Create 2d histogram (density) plot (manually).
+################################################################################
 n = 10000
 x, y, pi, relerr = calc_pi(n)
 
@@ -45,8 +49,10 @@ plt.ylim([0,1])
 plt.axes().set_aspect('equal')
 plt.savefig('plot_complicated_MC_pi_density.png')
 
+################################################################################
 # Create density plotting function to easily reproduce plots with common
 # styling.  Returns a plot object.
+################################################################################
 def my2dhist(x, y, title = '', nbins_x = 25, nbins_y = 25):
     H, xp, yp = np.histogram2d(x, y, bins=[nbins_x, nbins_y])
     H = np.ma.masked_where(H == 0, H)
@@ -67,6 +73,9 @@ def my2dhist(x, y, title = '', nbins_x = 25, nbins_y = 25):
 
     return(plt)
 
+################################################################################
+# Use the function above to create plots for a variety of sample sizes.
+################################################################################
 xe2, ye2, pie2, relerre2 = calc_pi(1e2)
 xe4, ye4, pie4, relerre4 = calc_pi(1e4)
 xe6, ye6, pie6, relerre6 = calc_pi(1e6)
@@ -79,7 +88,9 @@ plte2.savefig('plot_complicated_MC_pi_density_1e2_pts.png')
 plte4.savefig('plot_complicated_MC_pi_density_1e4_pts.png')
 plte6.savefig('plot_complicated_MC_pi_density_1e6_pts.png')
 
+################################################################################
 # Create a line plot showing convergence as n increases.
+################################################################################
 
 samples = [1e2, 1e3, 1e4, 1e5, 1e6, 1e7]
 x   = np.empty(shape=(0))
@@ -106,5 +117,4 @@ plt.ylabel(r'Estimated Value Of $\pi\pm2\sigma$ Versus Actual')
 
 plt.savefig('plot_complicted_MC_pi_convergence.png')
 plt.show()
-
 
--- Monte_Carlo_Pi/MC_pi_simple.py
+++ Monte_Carlo_Pi/MC_pi_simple.py
@@ -2,7 +2,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # Function to calculate pi by comparing if a random x,y pair falls within a
-# circle with unit radius.  Return the x,y values and estimated value of pi.
+# circle with unit radius.  Return the x,y values, estimate of pi, and relative
+# error.
 def calc_pi(n):
     x = np.random.rand(n)
     y = np.random.rand(n)
@@ -11,19 +12,22 @@ def calc_pi(n):
     relerr = np.sqrt((4/np.pi - 1) / n)
     return(x, y, pi, relerr)
 
+# This conditional is used to run a simulation and create a simple plot if this
+# script is called directly.  However, it is not run if this script is included
+# in another file (allowing us to re-use the function defined above elsewhere).
 if __name__ == '__main__':
 
     # Create scatter plot of individual points.
     n = 2500
     x, y, pi, err = calc_pi(n)
     fig = plt.figure()
-    
+
     plt.scatter(x, y, marker='x', color='k')
-    
+
     plt.xlim([0,1])
     plt.ylim([0,1])
     plt.title(r'$\pi$ estimated as {:.5f} using {:d} samples'.format(pi, n))
     plt.axes().set_aspect('equal')
     plt.savefig('plot_simple_MC_pi_points.png')
     plt.show()
-    
+
